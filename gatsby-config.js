bf8df9a3b70e812d10f6b9987f180e82b825caf7
Matthew Pagan <mastapegs01@gmail.com>
2020-05-23 15:46:55 -0400

    installed shopify dependenies

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,15 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-shopify`,
+      options: {
+        // The domain name of your Shopify shop.
+        shopName: `amandaz-dealz.myshopify.com`,
+        // The storefront access token
+        accessToken: `7bb072426eb538dc4f6a913355f95c24`,
+      },
+    },
+  ],
 }
