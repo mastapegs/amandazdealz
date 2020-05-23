23d6dffd4e30fbe8505ae90f79f09ad439f33d57
Matthew Pagan <mastapegs01@gmail.com>
2020-05-23 15:53:17 -0400

    successful queries for data

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import PageTitle from '../components/PageTitle'
+import { graphql } from "gatsby"
 
-const Shop = () => {
+export const query = graphql`
+  {
+    allShopifyProduct(sort: { fields: [title] }) {
+      edges {
+        node {
+          title
+          shopifyId
+          description
+          handle
+          priceRange {
+            minVariantPrice {
+              amount
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const Shop = ({ data }) => {
   return (
     <>
       <PageTitle titleText='Shop' />
+      <ul>
+        {data.allShopifyProduct.edges.map(({ node }) => (
+          <li key={node.shopifyId}>
+            <h3>
+              {node.title}
+              {" - "}${Number(node.priceRange.minVariantPrice.amount).toFixed(2)}
+            </h3>
+            <p>{node.description}</p>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
